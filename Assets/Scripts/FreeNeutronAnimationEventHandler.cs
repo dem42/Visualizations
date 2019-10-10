212b96ed8a562faa7f8eca1424fdcc7f85922dbd
martin <martin.papanek@gmail.com>
2019-10-10 23:52:52 +0200

    animation events

---
+++ Assets/Scripts/FreeNeutronAnimationEventHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreeNeutronAnimationEventHandler : MonoBehaviour
+{
+    
+    void HideNeutronWhenAbsorbed() {
+        Debug.Log("Animation event callback received");
+        gameObject.SetActive(false);
+    }
+}
