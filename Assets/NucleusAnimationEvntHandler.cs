e9edc9932f6fc3bf7b2ff4abb112419f07e4e637
martin <martin.papanek@gmail.com>
2019-11-02 23:55:52 +0100

    animation tutorial and particle system for boom

---
+++ Assets/NucleusAnimationEvntHandler.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NucleusAnimationEvntHandler : MonoBehaviour
+{
+    void PlayOnCollision(int ok) {
+        Debug.Log($"Collision just happened {ok}");
+    }
+}
